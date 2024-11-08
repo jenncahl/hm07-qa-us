6cf6e28d302466e9e78403ca79d5a3f72df4533e
Jennifer <jennifer.sokolowska95@gmail.com>
2024-11-08 10:35:22 +0200

    Project 7

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-62b31b7a-c9ba-43ef-931e-57250242d187.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-3aec7c91-6196-4d52-8636-b549d669371d.containerhub.tripleten-services.com"
 }
